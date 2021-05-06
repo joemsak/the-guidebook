9490e2c350f16a8e9bc6a1d98a8173e19936604d
Joe Sak <joe@joesak.com>
2021-05-06 18:58:38 -0500

    Sign in when signing up

--- app/controllers/coach/registrations_controller.rb
+++ app/controllers/coach/registrations_controller.rb
@@ -2,4 +2,21 @@ class Coach::RegistrationsController < ApplicationController
   def new
     @coach = Coach.new
   end
+
+  def create
+    @coach = Coach.new(coach_params)
+
+    if @coach.save
+      sign_in(@coach)
+      redirect_to @coach, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  private
+  def coach_params
+    params.require(:coach)
+      .permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
+
   get :signin, to: 'sessions#new', as: :signin
   get :login, to: 'sessions#new', as: :login
+
   match :signout, via: %i[get delete], to: 'sessions#destroy', as: :signout
   match :logout, via: %i[get delete], to: 'sessions#destroy', as: :logout
 
