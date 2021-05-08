cc427674421f7281fe720cda8748f8bcc380a37b
Joe Sak <joe@joesak.com>
2021-05-08 00:33:28 -0500

    Add .btn to the safelist, I guess. It's used but gets purged

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  purge: [
-    "./app/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/javascript/**/*.vue",
-  ],
+  purge:  {
+    content: [
+      "./app/**/*.html.erb",
+      "./app/helpers/**/*.rb",
+      "./app/javascript/**/*.js",
+      "./app/javascript/**/*.vue",
+    ],
+    safelist: ['btn'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
