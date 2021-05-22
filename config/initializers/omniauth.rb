b352349f6b7bb24d5c8268c80d1bb2fd98bc69f8
Joe Sak <joe@joesak.com>
2021-05-22 00:38:55 -0500

    Allow empty calendly keys in omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :calendly,
-    Rails.application.credentials.calendly[:client_id],
-    Rails.application.credentials.calendly[:client_secret]
+    Rails.application.credentials.dig(:calendly, :client_id),
+    Rails.application.credentials.dig(:calendly, :client_secret)
 end
