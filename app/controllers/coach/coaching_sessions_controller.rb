536b02f021cdedc7db00e4f6f06245e5cbdb5a43
Joe Sak <joe@joesak.com>
2021-05-29 16:57:43 -0500

    Fix up lingering issues from changes

--- app/controllers/coach/coaching_sessions_controller.rb
+++ app/controllers/coach/coaching_sessions_controller.rb
@@ -12,7 +12,7 @@ class Coach::CoachingSessionsController < CoachController
       .build(coaching_session_params)
 
     if @coaching_session.save
-      redirect_to [:coach, @coaching_session], notice: t('.success')
+      redirect_to [:coach, @coaching_session], notice: t(".success")
     else
       render :new
     end
@@ -24,18 +24,13 @@ class Coach::CoachingSessionsController < CoachController
 
   def edit
     @coaching_session = current_coach.coaching_sessions.find(params[:id])
-
-    @coaching_session.duration_complex = view_context.pluralize(
-      @coaching_session.duration,
-      @coaching_session.duration_unit.singularize
-    )
   end
 
   def update
     @coaching_session = current_coach.coaching_sessions.find(params[:id])
 
     if @coaching_session.update(coaching_session_params)
-      redirect_to [:coach, @coaching_session], notice: t('.success')
+      redirect_to [:coach, @coaching_session], notice: t(".success")
     else
       render :edit
     end
@@ -45,28 +40,13 @@ class Coach::CoachingSessionsController < CoachController
     @coaching_session = current_coach.coaching_sessions.find(params[:id])
     @coaching_session.destroy
     redirect_to [:coach, :coaching_sessions],
-      notice: t('.success', name: @coaching_session.client_name)
+      notice: t(".success", name: @coaching_session.client_name)
   end
 
   private
+
   def coaching_session_params
     params.require(:coaching_session)
-      .permit(:client_profile_id, :starts_at, :video_embed, :notes)
-      .merge(
-        duration: duration,
-        duration_unit: duration_unit
-      )
-  end
-
-  def duration
-    duration_complex.scan(/\d+/)[0].to_i
-  end
-
-  def duration_unit
-    duration_complex.scan(/[a-z]+/)[0].pluralize
-  end
-
-  def duration_complex
-    params[:coaching_session][:duration_complex]
+      .permit(:client_profile_id, :start_time, :video_embed, :notes)
   end
 end
--- spec/factories/coaching_sessions.rb
+++ spec/factories/coaching_sessions.rb
@@ -2,8 +2,18 @@ FactoryBot.define do
   factory :coaching_session do
     client_profile
     coach_profile
-    starts_at { 1.week.from_now }
-    duration { 1 }
-    duration_unit { :hours }
+    sequence(:external_id) { |n| "event-uuid-#{n}" }
+    sequence(:external_url) { |n| "https://example-events.com/event/#{n}" }
+    sequence(:name) { |n| "Event ##{n}" }
+    external_status { "active" }
+    start_time { 2.days.from_now }
+    end_time { 2.days.from_now + 1.hour }
+    external_created_at { 1.week.ago }
+    external_updated_at { 1.week.ago }
+    event_type { {} }
+    location { {} }
+    invitees_counter_total { 1 }
+    invitees_counter_active { 1 }
+    invitees_counter_limit { 1 }
   end
 end
